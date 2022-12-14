5052898f29b68579711131e9bd70604eaa63197a
Ynam12 <107596502+Ynam12@users.noreply.github.com>
2022-12-14 09:45:06 -0600

    Update video.py

--- video.py
+++ video.py
@@ -1,19 +1,15 @@
 # importing the module
 import pywhatkit
 import random
+def play():
+    a = "https://www.youtube.com/watch?v=oZQhKgvUyYE"
+    b = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    c = []
+    c.append(a)
+    c.append(b)
+    try:
+        pywhatkit.playonyt(c[random.randint(0,1)])
+        print("Playing...")
  
-a = "https://www.youtube.com/watch?v=oZQhKgvUyYE"
-b = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-c = []
-c.append(a)
-c.append(b)
-try:
-   
-
-  pywhatkit.playonyt(c[random.randint(0,1)])
-  print("Playing...")
- 
-except:
-   
-  # printing the error message
-  print("Network Error Occurred")
+    except:
+        print("Network Error Occurred")
